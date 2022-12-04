d3d7437f980c0004b144a14166ca00580b953323
Trident Apollo <68052236+TriDEntApollO@users.noreply.github.com>
2022-12-04 22:10:08 +0530

    Implemented color coding

--- sources/functions/var/globals.py
+++ sources/functions/var/globals.py
@@ -1,4 +1,15 @@
 def initialize():
+    global p, c, d, bl, g, y, r, b, u, e
+    p = '\033[95m'
+    c = '\033[96m'
+    d = '\033[36m'
+    bl = '\033[94m'
+    g = '\033[92m'
+    y = '\033[93m'
+    r = '\033[91m'
+    b = '\033[1m'
+    u = '\033[4m'
+    e = '\033[0m'
     global all_conns, active_conns, till, accept, server
     global host, port
     all_conns = {}
